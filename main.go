b650a497c35345a2a5503ddaca8fb15ef6d26869
Justin <justin@garofolo.net>
2022-07-17 10:59:41 -0400

    additional cfg vars, invoke login + extract PRs

--- main.go
+++ main.go
@@ -4,22 +4,42 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+
+	pr_gh "github.com/ooojustin/pr-puller/pkg/github"
 )
 
 func main() {
-	accBytes, err := ioutil.ReadFile("./account.cfg")
+	cfgBytes, err := ioutil.ReadFile("./config.json")
 	if err != nil {
 		panic("failed to read account file")
 	}
 
-	var account map[string]interface{}
-	err = json.Unmarshal(accBytes, &account)
+	var config map[string]interface{}
+	err = json.Unmarshal(cfgBytes, &config)
 	if err != nil {
 		panic("failed to parse account credentials")
 	}
 
-	username := account["username"].(string)
-	password := account["password"].(string)
+	username := config["username"].(string)
+	password := config["password"].(string)
+	org := config["org"].(string)
+	saveCookies := config["save_cookies"].(bool)
 
-	fmt.Println(username, password)
+	ghc, ok := pr_gh.NewGithubClient(username, password, saveCookies)
+	if !ok {
+		panic("failed to initialize github client")
+	}
+
+	var login bool = ghc.Login()
+	if !login {
+		print("login: ", login)
+	}
+
+	var pullRequests []*pr_gh.PullRequest
+	ghc.GetPullRequests(org, true, &pullRequests)
+
+	fmt.Println("LOADED PRS:", len(pullRequests))
+	for _, pr := range pullRequests {
+		fmt.Println(pr.Title, pr.Href, pr.Labels)
+	}
 }
