cb23bc499c83b24fe91b746ea6e4b494e255d956
Justin <justin@garofolo.net>
2022-08-11 13:36:31 -0400

    util funcs to read console input

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -1,15 +1,41 @@
 package utils
 
 import (
+	"bufio"
 	"fmt"
 	"io"
 	"mime/multipart"
 	"net/http"
+	"os"
 	"regexp"
 	"strings"
+	"syscall"
 
+	"golang.org/x/crypto/ssh/terminal"
 	"golang.org/x/net/html"
 )
+
+func ReadString(prompt string) (string, error) {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Printf("%s: ", prompt)
+	response, err := reader.ReadString('\n')
+	if err != nil {
+		return "", err
+	}
+	return strings.TrimSpace(response), nil
+}
+
+func ReadPassword(prompt string) (string, error) {
+	var stdin int = int(syscall.Stdin)
+	fmt.Printf("%s: ", prompt)
+	pwdBytes, err := terminal.ReadPassword(stdin)
+	if err != nil {
+		return "", err
+	}
+	fmt.Println()
+	pwd := string(pwdBytes)
+	return strings.TrimSpace(pwd), nil
+}
 
 func GetResponseBody(r *http.Response) (string, bool) {
 	defer r.Body.Close()
